037c8c84ec9fba6d0e1d43aa753746f844b22142
Lethanial Leveille <leveillelethanial@gmail.com>
2025-11-16 12:45:31 -0500

    modified calculator p1 - added base functions

--- calculator.py
+++ calculator.py
@@ -1,11 +1,32 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/Lethanial-Leveille/lab11-LL-DW.git
+# Partner 1: Lethanial Leveille
+# Partner 2: Dylan Wells
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def logarithm(a, b):
+    # log base a of b
+    if a <= 0 or a == 1:
+        raise ValueError("Invalid base for logarithm")
+    if b <= 0:
+        raise ValueError("Invalid argument for logarithm")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
 
 
